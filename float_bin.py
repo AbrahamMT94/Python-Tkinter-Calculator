62b05f56d782730a07d719a7243f777bb4b0e0a4
AbrahamMT94 <48168165+AbrahamMT94@users.noreply.github.com>
2019-03-07 21:12:06 -0600

    Python Tkinter Programming Calculator

--- float_bin.py
+++ float_bin.py
@@ -1,14 +1,27 @@
-def float_bin(number): 
+def float_bin(number,precision=4): 
     #split the number
-    whole, dec = str(float(number)).split(".") 
+   whole, dec = str(float(number)).split(".") 
     
-    #convert all vals to integers
-    whole = int(whole) 
-    dec = int (dec) 
-   # turn to string and format 
-    whole = '{:b}.'.format(whole)
-    dec = '{:b}'.format(dec)
-    dec = dec[::-1]
-    result = whole + dec
-    return result
+    #convert whole to binary
+   whole = '{:b}'.format(int(whole))
+   dec = int (dec) 
+   #check ifleft over is 0 and if so retun number 
+   if dec==0:
+      return whole
+   seen_values=[]
+   current=dec
+   point='.'
+   # the loop below keeps adding until precision is met or it loops over
+   while current not in seen_values and current!=0 and precision!=0 :
+      seen_values.append(dec)
+      dec=dec*2
+      if dec>=100:
+         point+='1'
+         dec-=100
+         current=dec
+      else :
+         point+='0'
+         current=dec
+      precision-=1     
 
+   return whole+point
